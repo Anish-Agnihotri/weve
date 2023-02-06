4cc7fb6c7ba025747f785c6adda354ce2a097707
dmac <samuraidan@gmail.com>
2023-02-06 12:57:51 -0800

    feat: 🔍 add a GQL method to retrieve inbox msgs

---
+++ src/utils/query.js
@@ -0,0 +1,56 @@
+import Arweave from 'arweave/web';
+const arweave = Arweave.init();
+
+export const getWeavemailTransactions = async (arweave, address, mailItemTxid) => {
+
+	var mailItemFilter = "";
+	if (mailItemTxid) {
+		mailItemFilter = `ids: ["${mailItemTxid}"],`;
+	}
+
+	var addressFilter = ""
+	if (address) {
+		addressFilter = `recipients: ["${address}"],`;
+	}
+
+	const res2 = await arweave.api.post('/graphql', {
+		query: `
+			{
+				transactions(first: 50, 
+					${addressFilter}
+					${mailItemFilter}
+					tags: [
+						{
+							name: "App-Name",
+							values: ["permamail"]
+						}
+					]
+				) {
+					edges {
+						node {
+							id
+							recipient
+							owner {
+								address
+							}
+							tags {
+								name
+								value
+							}
+							block {
+								timestamp
+								height
+							}
+							quantity {
+								winston
+							}
+							fee {
+								winston
+							}
+						}
+					}
+				}
+			}`,
+	});
+	return res2.data;
+}
